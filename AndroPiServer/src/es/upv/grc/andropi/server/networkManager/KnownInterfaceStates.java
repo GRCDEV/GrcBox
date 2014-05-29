cf320c22d728e87e887fe957ffd6bd1584a80606
Subhadeep <subhadeep@subhadeep-MS-7758.(none)>
2014-05-29 19:10:17 +0200

    restructured nmcli code

---
+++ AndroPiServer/src/es/upv/grc/andropi/server/networkManager/KnownInterfaceStates.java
@@ -0,0 +1,8 @@
+package es.upv.grc.andropi.server.networkManager;
+
+public class KnownInterfaceStates {
+	
+	public static final String connected = "connected";
+	public static final String disconnected = "disconnected";
+	public static final String unmanaged = "unmanaged";
+}
