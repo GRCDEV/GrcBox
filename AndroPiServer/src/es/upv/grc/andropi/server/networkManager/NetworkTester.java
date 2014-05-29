112793e8cbde21598641001fbc864ea12c57b840
Subhadeep <subhadeep@subhadeep-MS-7758.(none)>
2014-05-29 19:47:37 +0200

    updated the tester class

--- AndroPiServer/src/es/upv/grc/andropi/server/networkManager/NetworkTester.java
+++ AndroPiServer/src/es/upv/grc/andropi/server/networkManager/NetworkTester.java
@@ -9,14 +9,17 @@ public class NetworkTester {
 			InterfaceManager.networkRestart();
 			InterfaceManager.updateNetworkInterfaceInformation();
 			LinkedList<NetworkInterface> ifaces = InterfaceManager.getNetworkInterfaceInformation();
-			for(int i = 0; i < ifaces.size(); i++)
-			{
-				System.out.println(ifaces.get(i).getInterfaceName() + " " +
+			if(ifaces != null)
+			{ // hope to have found some devices
+				for(int i = 0; i < ifaces.size(); i++)
+				{
+					System.out.println(ifaces.get(i).getInterfaceName() + " " +
 									ifaces.get(i).getInterfaceType() + " " +
 									ifaces.get(i).getInterfaceState() + " " +
 									ifaces.get(i).isIP4AddressAvailable() + " " +
 									ifaces.get(i).getInterfaceIP4() + " " +
 									ifaces.get(i).getGatewayIP4() + " ");
+				}
 			}
 		}
 		catch(Exception e)
