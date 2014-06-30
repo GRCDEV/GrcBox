17ec1ba6ec586ae628bab14c4fc2b28cdd3f0766
Subhadeep <subhadeep.official@gmail.com>
2014-06-30 17:19:46 +0200

    Adding Required files for previous version

---
+++ GrcBoxCommon/src/es/upv/grc/grcbox/common/KnownWifiModes.java
@@ -0,0 +1,14 @@
+package es.upv.grc.grcbox.common;
+
+
+/**
+ * Write a description of class KnownWifiModes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KnownWifiModes
+{
+    public static final String adhoc = "adhoc";
+    public static final String infrastructure = "infrastructure";
+}
