c022421ff82c0218e6bca0c0740823fe09b1cbe2
sertinell <sertinell@gmail.com>
2015-05-29 18:11:13 +0200

    Everything was removed

--- NMDBusIface/src/org/freedesktop/NetworkManager/Constants/NM_BT_CAPABILITIES.java
+++
@@ -1,8 +0,0 @@
-package org.freedesktop.NetworkManager.Constants;
-
-import org.freedesktop.dbus.UInt32;
-public class NM_BT_CAPABILITIES{
-public static final UInt32 NONE= new UInt32(0x0);
-public static final UInt32 DUN= new UInt32(0x1);
-public static final UInt32 PAN= new UInt32(0x2);
-}
